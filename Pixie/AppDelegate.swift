eb7c85eadf2f09dee30e662a8c2cb215a478f750
IsaacXen <blackoutxen@gmail.com>
2019-10-11 18:33:26 +0800

    Initial Commit

---
+++ Pixie/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pixie
+//
+//  Created by Isaac Chen on 2019/10/11.
+//  Copyright © 2019 ix4n33. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
