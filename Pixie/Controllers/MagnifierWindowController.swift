b24cf3b63fb4ff414491971f1f02796365087bdb
IsaacXen <blackoutxen@gmail.com>
2019-10-22 22:23:37 +0800

    get rid of auto-hiding titlebar

--- Pixie/Controllers/MagnifierWindowController.swift
+++ Pixie/Controllers/MagnifierWindowController.swift
@@ -2,8 +2,6 @@ import Cocoa
 
 /// The window controller of the main magnifier window.
 class MagnifierWindowController: NSWindowController, NSWindowDelegate {
-
-//    private var _willPresentSheet: Bool = false
     
     // MARK: - Loading Window
     
@@ -12,10 +10,8 @@ class MagnifierWindowController: NSWindowController, NSWindowDelegate {
         window?.minSize = NSSize(width: 300, height: 300)
         window?.collectionBehavior = [.init(rawValue: 0), .managed, .participatesInCycle, .fullScreenPrimary]
         window?.delegate = self
+        window?.title = "Pixie"
         window?.tabbingMode = .disallowed
-        window?.isMovableByWindowBackground = true
-        window?.titleVisibility = .hidden
-        window?.titlebarAppearsTransparent = true
     }
     
     override func windowDidLoad() {
@@ -27,39 +23,6 @@ class MagnifierWindowController: NSWindowController, NSWindowDelegate {
         
         let shouldFloat = DefaultsController.shared.retrive(.floatingMagnifierWindow)
         window?.level = shouldFloat ? .floating : .normal
-        
-        // setup tracking area
-        updateTrackingAreas()
-        
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + DispatchTimeInterval.seconds(3)) { [weak self] in
-            if !(self?.window?.frame.contains(NSEvent.mouseLocation) ?? false) {
-                self?._titleBar?.animator().alphaValue = 0
-            }
-        }
-    }
-    
-    func windowDidEndLiveResize(_ notification: Notification) {
-        updateTrackingAreas()
-    }
-
-    func windowWillEnterFullScreen(_ notification: Notification) {
-        updateTrackingAreas()
-    }
-    
-    func windowDidEnterFullScreen(_ notification: Notification) {
-        window?.titleVisibility = .visible
-        window?.titlebarAppearsTransparent = false
-        _titleBar?.alphaValue = 1
-    }
-    
-    func windowWillExitFullScreen(_ notification: Notification) {
-        window?.titleVisibility = .hidden
-        window?.titlebarAppearsTransparent = true
-        _titleBar?.alphaValue = 0
-    }
-    
-    func windowDidExitFullScreen(_ notification: Notification) {
-        updateTrackingAreas()
     }
     
     func windowWillClose(_ notification: Notification) {
@@ -67,57 +30,6 @@ class MagnifierWindowController: NSWindowController, NSWindowDelegate {
             NSApp.terminate(nil)
         }
     }
-    
-    // MARK: - Tracking Area
-
-    private var _trackingArea: NSTrackingArea?
-    
-    private func updateTrackingAreas() {
-        guard
-            let window = window,
-            let contentView = window.contentView
-        else { return }
-        
-        if let tr = _trackingArea {
-            contentView.removeTrackingArea(tr)
-        }
-        
-        if !window.styleMask.contains(.fullScreen) {
-            _trackingArea = NSTrackingArea(rect: contentView.frame, options: [.activeInActiveApp, .mouseEnteredAndExited], owner: self, userInfo: nil)
-            contentView.addTrackingArea(_trackingArea!)
-        }
-    }
-    
-    // MARK: - Event Handling
-    
-    // 1. Show or hide window titlebar when mouse entered and exited.
-    
-    override func mouseEntered(with event: NSEvent) {
-        _titleBar?.animator().alphaValue = 1
-        super.mouseEntered(with: event)
-    }
-    
-    override func mouseExited(with event: NSEvent) {
-        _titleBar?.animator().alphaValue = 0
-        super.mouseExited(with: event)
-    }
-    
-    private var _titleBar: NSView? {
-        window?.standardWindowButton(.closeButton)?.superview?.superview
-    }
-    
-//    func window(_ window: NSWindow, willPositionSheet sheet: NSWindow, using rect: NSRect) -> NSRect {
-//        print(#function, window, sheet, rect)
-//
-//        _willPresentSheet = true
-//        updateTrackingAreas()
-//
-//        window.animator().titleVisibility = .visible
-//        window.animator().titlebarAppearsTransparent = false
-//        _titleBar?.animator().alphaValue = 1
-//
-//        return NSMakeRect(rect.minX, rect.maxY - 22, rect.width, 0)
-//    }
     
     // MARK: - Init
     
